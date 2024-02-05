db1090b38340356273a4d92dc5e18ca6e50b2dd2
Victor Almeida de Freitas <victor.freitas08@hotmail.com>
2024-02-04 23:36:52 -0300

    update server.js to clean code

--- src/server.js
+++ src/server.js
@@ -3,47 +3,54 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import { v4 as uuidv4 } from "uuid";
 
-const app = express();
+const DEFAULT_PORT = 3000;
 
-const httpServer = createServer(app);
+class ChatServer {
+  constructor(port = DEFAULT_PORT) {
+    this.app = express();
+    this.httpServer = createServer(this.app);
+    this.io = new Server(this.httpServer);
+    this.createdRooms = [];
+    this.port = port;
+  }
 
-const io = new Server(httpServer, {
-  // opções do socket.io se necessário
-});
+  start() {
+    this.io.on("connection", (socket) => this.setupSocketHandlers(socket));
+    this.httpServer.listen(this.port, () => console.log(`Servidor rodando na porta ${this.port}`));
+  }
 
-const createdRooms = [];
+  setupSocketHandlers(socket) {
+    console.log(`Um usuário se conectou no pool de default ${socket.id}`);
+    socket.on("createRoom", (roomName) => this.handleCreateRoom(socket, roomName));
+    socket.on("joinRoom", (roomName) => this.handleJoinRoom(socket, roomName));
+    socket.on("sendMessage", (data) => this.handleSendMessage(socket, data));
+    socket.on("listRooms", () => this.handleListRooms(socket));
+    socket.on("disconnect", () => this.handleDisconnect(socket));
+  }
 
-io.on("connection", (socket) => {
-  console.log(`Um usuário se conectou no pool de default ${socket.id}`);
-
-  socket.on("createRoom", (roomName) => {
+  handleCreateRoom(socket, roomName) {
     const newRoom = roomName || uuidv4();
-    createdRooms.push(newRoom);
+    this.createdRooms.push(newRoom);
     socket.emit("roomCreated", newRoom);
-    console.log(`Sala ${newRoom} criada`);
-  });
+  }
 
-  socket.on("joinRoom", (roomName) => {
+  handleJoinRoom(socket, roomName) {
     socket.join(roomName);
     socket.emit("message", `Você entrou na sala ${roomName}`);
-    console.log(`Usuário ${socket.id} entrou na sala ${roomName}`);
-  });
+  }
 
-  socket.on("sendMessage", (data) => {
-    io.to(data.room).emit("message", {
-      room: data.room,
-      message: data.message,
-    });
-  });
+  handleSendMessage(socket, data) {
+    this.io.to(data.room).emit("message",{ room: data.room, message: data.message });
+  }
 
-  socket.on("listRooms", () => {
-    socket.emit("roomsList", createdRooms);
-  });
+  handleListRooms(socket) {
+    socket.emit("roomsList", this.createdRooms);
+  }
 
-  socket.on("disconnect", () => {
+  handleDisconnect(socket) {
     console.log("Um usuário se desconectou");
-  });
-});
+  }
+}
 
-const PORT = process.env.PORT || 3000;
-httpServer.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+const chatServer = new ChatServer();
+chatServer.start();
